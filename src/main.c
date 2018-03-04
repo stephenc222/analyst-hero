a9249a66f7cbd80d1fcc955f1bcb78a3643a1d3f
Stephen Collins <stephenc211@gmail.com>
2018-03-04 02:39:52 -0600

    fixed text rendering memory leak, progress on title scene

--- src/main.c
+++ src/main.c
@@ -6,28 +6,135 @@
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
-const SDL_Color White = {255, 255, 255};  
+const int CENTER_WIDTH = SCREEN_WIDTH / 2;
+const int CENTER_HEIGHT = SCREEN_HEIGHT / 2;
+const SDL_Color WHITE = {255, 255, 255};  
 const SDL_Color BLACK = {0, 0, 0}; 
+
+typedef struct {
+  int fontSize;
+  TTF_Font* font;
+  SDL_Rect rect;
+  char* text;
+} TextRect;
+
+typedef struct {
+  int reportAmmo;
+  int coffee;
+} Inventory;
+
+typedef struct {
+  int health;
+  int status;
+  int timeScore;
+  int reportEff;
+  Inventory inventory;
+} Player;
+
+typedef struct {
+  int health;
+  int status;
+  char* name;
+} Boss;
+
+typedef struct {
+  int startX, startY;
+  int endX, endY;
+} NPC;
 
 typedef struct {
   char* text;
   TTF_Font* font;
+  int numSceneRects;
+  TextRect* currentScene;
+  Player* player;
+  Boss* firstBoss;
+  Boss* finalBoss;
+  NPC* maleOne;
+  NPC* femaleOne;
+  NPC* femaleTwo;
 } Game;
 
 // Forward Declarations
 Game* newGame(char* text);
-
 int initGame(void);
 
+void loadTitleScene(Game* game);
+void loadPlayScene(Game* game, SDL_Rect titleSceneRects[]);
+void loadFightScene(Game* game, SDL_Rect titleSceneRects[]);
+void loadGameoverScene(Game* game, SDL_Rect titleSceneRects[]);
+void loadGameWinScene(Game* game, SDL_Rect titleSceneRects[]);
+
 void cleanUp(SDL_Renderer *renderer,SDL_Window *window);
-
 TTF_Font* loadText(const char *fileName, int fontSize);
-
 void renderText(SDL_Renderer *renderer,TTF_Font *font,SDL_Rect rect, char *text);
-
 void render(SDL_Renderer *renderer, Game* game);
-
 void renderScene(SDL_Renderer *renderer,Game* game);
+void updateScene(SDL_Renderer *renderer, Game* game);
+
+// function definitions
+void loadTitleScene(Game* game) {
+    // title scene
+  SDL_Rect titleRect;
+  SDL_Rect subTitleRect;
+  SDL_Rect startGameRect;
+
+  TextRect* titleTextRect = malloc(sizeof *titleTextRect);
+  TextRect* subTitleTextRect = malloc(sizeof *subTitleTextRect);
+  TextRect* startGameTextRect = malloc(sizeof *startGameTextRect);
+
+  char* titleText =  "Analyst Hero";
+  char* subText =  "Click to start game";
+  char* startText =  "START GAME";
+
+  titleRect.x = CENTER_WIDTH - 100;
+  titleRect.y = CENTER_HEIGHT - 150;
+  titleRect.w = 200;
+  titleRect.h = 100;
+  titleTextRect->text = titleText;
+  titleTextRect->rect = titleRect;
+  titleTextRect->font = loadText("../assets/OpenSans-Bold.ttf", 100);
+
+  subTitleRect.x = CENTER_WIDTH - 100;
+  subTitleRect.y = CENTER_HEIGHT - 50 ;
+  subTitleRect.w = 200;
+  subTitleRect.h = 60;
+  subTitleTextRect->text = subText;
+  subTitleTextRect->rect = subTitleRect;
+  subTitleTextRect->font = loadText("../assets/OpenSans-Bold.ttf", 40);
+
+  startGameRect.x = CENTER_WIDTH - 100;
+  startGameRect.y = CENTER_HEIGHT;
+  startGameRect.w = 200;
+  startGameRect.h = 70;
+  startGameTextRect->text = startText;
+  startGameTextRect->rect = startGameRect;
+  startGameTextRect->font = loadText("../assets/OpenSans-Bold.ttf", 70);
+
+  TextRect* titleSceneRects[3] = {titleTextRect, subTitleTextRect, startGameTextRect};
+  game->numSceneRects = 3;
+
+  for (int i = 0; i < 3; ++i) {
+    game->currentScene[i] = *titleSceneRects[i];
+    printf("test again: %s \n", game->currentScene[i].text);
+  }
+}
+
+void loadPlayScene(Game* game, SDL_Rect playSceneRects[]) {
+  // TODO: main play scene
+}
+
+void loadFightScene(Game* game, SDL_Rect titleSceneRects[]) {
+  // TODO: fight scene
+}
+
+void loadGameoverScene(Game* game, SDL_Rect titleSceneRects[]) {
+    // TODO: Gameover scene
+}
+
+void loadGameWinScene(Game* game, SDL_Rect titleSceneRects[]) {
+    // TODO: GameWin scene
+}
 
 // Definitions
 void renderText(SDL_Renderer *renderer,TTF_Font *font,SDL_Rect rect, char *text) {
@@ -37,21 +144,33 @@ void renderText(SDL_Renderer *renderer,TTF_Font *font,SDL_Rect rect, char *text)
   SDL_FreeSurface(surfaceMessage);
 
   SDL_RenderCopy(renderer, Message, NULL, &rect); 
+  SDL_DestroyTexture(Message);
+}
+
+void updateScene(SDL_Renderer *renderer, Game* game) {
+  // TODO:
 }
 
 void renderScene(SDL_Renderer *renderer, Game* game) {
   // render scene specific stuff here
-    // TODO: move this to a better place
-  SDL_Rect rect;
-  rect.x = 0;  //controls the rect's x coordinate 
-  rect.y = 0; // controls the rect's y coordinte
-  rect.w = 100; // controls the width of the rect
-  rect.h = 100; // controls the height of the rect
-  renderText(renderer, game->font,  rect, game->text);
+  // SDL_Rect rect;
+  // rect.x = 0;  //controls the rect's x coordinate 
+  // rect.y = 0; // controls the rect's y coordinte
+  // rect.w = 200; // controls the width of the rect
+  // rect.h = 100; // controls the height of the rect
+  for (int i = 0; i < game->numSceneRects; ++i) {
+    renderText(
+      renderer, 
+      game->currentScene[i].font,  
+      game->currentScene[i].rect, 
+      game->currentScene[i].text
+    );
+  }
+  // renderText(renderer, game->font,  rect, game->text);
 }
 
 TTF_Font* loadText(const char *fileName, int fontSize) {
-  TTF_Font* font = TTF_OpenFont(fileName, 24); //this opens a font style and sets a size
+  TTF_Font* font = TTF_OpenFont(fileName, fontSize); //this opens a font style and sets a size
   return font;
 }
 
@@ -61,10 +180,27 @@ Game* newGame(char* text) {
 
   // *note to self = must allocate (malloc) memory for structs
   Game* game = malloc(sizeof *game);
+  Inventory* inventory = malloc(sizeof *inventory);
+  Player* player = malloc(sizeof *player);
+  Boss* firstBoss = malloc(sizeof *firstBoss);
+  Boss* finalBoss = malloc(sizeof *finalBoss);
+  NPC* maleOne = malloc(sizeof *maleOne);
+  NPC* femaleOne = malloc(sizeof *femaleOne);
+  NPC* femaleTwo = malloc(sizeof *femaleTwo);
   // struct st *x = malloc(sizeof *x); 
-  TTF_Font* font = loadText("../assets/OpenSans-Regular.ttf",24);
+
+  TTF_Font* font = loadText("../assets/OpenSans-Bold.ttf", 100);
+
   game->text = text;
   game->font = font;
+  game->player = player;
+  game->firstBoss = firstBoss;
+  game->finalBoss = finalBoss;
+  game->maleOne = maleOne;
+  game->femaleOne = femaleOne;
+  game->femaleTwo = femaleTwo;
+  game->currentScene  = (TextRect*) malloc(sizeof(TextRect*)*10);
+
   return game;
 }
 
@@ -101,8 +237,8 @@ int initGame(void) {
   
   SDL_Window *window = SDL_CreateWindow(
     "Analyst Hero - Alpha", 
-    100, 
-    100, 
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED, 
     SCREEN_WIDTH, 
     SCREEN_HEIGHT, 
     SDL_WINDOW_RESIZABLE
@@ -124,6 +260,8 @@ int initGame(void) {
 
   // TODO: refactor game struct initializer
   Game* game = newGame("SOME TITLE");
+  loadTitleScene(game);
+  // init scenes
 
   // main game loop
   int loop = 1;
@@ -131,6 +269,7 @@ int initGame(void) {
     SDL_Event event;
     while(SDL_PollEvent(&event))
     {
+      // handle user events
       if(event.type == SDL_QUIT) {
         loop = 0;
         free(game);
@@ -138,6 +277,7 @@ int initGame(void) {
         return 0;
       }
     }
+    // update
     render(renderer, game);
   }
   
